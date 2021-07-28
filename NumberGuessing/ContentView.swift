c870292c6c317482e5f63df27fc39547c016620a
Tim Randall <trtimrandall@gmail.com>
2021-07-28 23:25:58 +1000

    Initial Commit

---
+++ NumberGuessing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NumberGuessing
+//
+//  Created by Tim Randall on 28/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NumberGuessing/NumberGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGuessingApp.swift
+//  NumberGuessing
+//
+//  Created by Tim Randall on 28/7/21.
+//
+
+import SwiftUI
+
+@main
+struct NumberGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
